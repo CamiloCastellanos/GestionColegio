786ef416c5bd864257b79a843fb809cf7e4bee76
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-01-30 13:24:32 -0500

    Creacion de componentes para Estudiante: "Actualizar Estudiante", "Adicionar Estudiante", "Lista Estudiante"

---
+++ FrontEnd/src/app/components/Estudiante/adicionar-estudiante/adicionar-estudiante.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-adicionar-estudiante',
+  templateUrl: './adicionar-estudiante.component.html',
+  styleUrls: ['./adicionar-estudiante.component.css']
+})
+export class AdicionarEstudianteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

8b00e8a25010adcdfa75b06cc7c2d521dfac7de7
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-02-04 12:45:55 -0500

    creacion de plantilla para adicionar estudiante

--- FrontEnd/src/app/components/Estudiante/adicionar-estudiante/adicionar-estudiante.component.ts
+++ FrontEnd/src/app/components/Estudiante/adicionar-estudiante/adicionar-estudiante.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-adicionar-estudiante',
@@ -10,6 +11,9 @@ export class AdicionarEstudianteComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(formulario: NgForm) {
   }
 
 }

d268370c9707bf88a6bdaeb84ebccbf83342a967
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-02-04 12:59:44 -0500

    Creacion de EstudianteModel

---
+++ Backend/Backend/Models/ProfesorModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class ProfesorModel
+    {
+        public int idProfesor { get; set; }
+        public int nombreProfesor { get; set; }
+        public int apellidoProfesor { get; set; }
+        public int fechaNacimientoProfesor { get; set; }
+        public int direccionProfesor { get; set; }
+        public int telefonoProfesor { get; set; }
+        public int identificacionProfesor { get; set; }
+        public AsignaturaModel[] asignaturaProfesor { get; set; }
+
+    }
+}

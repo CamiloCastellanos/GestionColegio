439fa18d1265bdc99a4edbcef346ecd0c8d4f031
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-02-04 11:15:34 -0500

    Creacion de AsignaturaModel

---
+++ Backend/Backend/Models/AsignaturaModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backend.Models
+{
+    public class AsignaturaModel
+    {
+        public int idAsignatura { get; set; }
+        public string codigoAsignatura { get; set; }
+        public string nombreAsignatura { get; set; }
+        
+    }
+}

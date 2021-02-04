fa4c8cd2f9c8b58cca454a464ada28402f6c066e
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-02-04 12:45:10 -0500

    Creacion de plantilla para adicionar asignaturas

--- FrontEnd/src/app/components/Asignatura/adicionar-asignaruta/adicionar-asignaruta.component.ts
+++ FrontEnd/src/app/components/Asignatura/adicionar-asignaruta/adicionar-asignaruta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-adicionar-asignaruta',
@@ -10,6 +11,9 @@ export class AdicionarAsignarutaComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(formulario: NgForm) {
   }
 
 }
